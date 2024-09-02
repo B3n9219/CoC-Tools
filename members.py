57bb874dd3c1856fdfb76a4c00cd2dc4d284de3c
B3n9219 <benkirk1441@gmail.com>
2024-09-02 18:07:36 +0100

    Added prints to members.py when getting the json errors

--- members.py
+++ members.py
@@ -8,8 +8,12 @@ import spreadsheet as sheet
 def get_clan_members_json_info():
     requestURL = clanRequestURL + clanTag + "/members"
     response = requests.get(requestURL, headers={"Authorization": "Bearer " + apiKey})
-    #print(response.json())
-    clanMemberInfo = response.json()["items"]
+    if not "items" in response.json():
+        clanMemberInfo = response.json()["items"]
+    else:
+        print(f"request URL: {requestURL}, status code: {response.status_code}, json: {response.json()}")
+        clanMemberInfo = None
+
     return clanMemberInfo
 
 
