ed39c8f37b982bf6223970b90e30406bdee41aba
B3n9219 <benkirk1441@gmail.com>
2024-09-14 22:28:06 +0100

    put creds in a seperate folder again

--- discord_bot/bot_setup.py
+++ discord_bot/bot_setup.py
@@ -4,11 +4,11 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
-#script_dir = os.path.dirname(os.path.abspath(__file__))
-#parent_dir = os.path.dirname(script_dir)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(script_dir)
 serviceKey_folder = r"\creds"
 #print(os.path.join(parent_dir, r'creds\ServiceKey_GoogleCloud.json'))
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(serviceKey_folder, 'ServiceKey_GoogleCloud.json')
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.path.join(parent_dir, r'creds\ServiceKey_GoogleCloud.json')
 
 # Load environment variables
 load_dotenv()
