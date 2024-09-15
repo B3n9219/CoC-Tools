60eef5c56b8665bc13a116532f64deec55edd9b9
B3n9219 <benkirk1441@gmail.com>
2024-09-15 19:08:20 +0100

    added response defer, so command doesn't time out

--- discord_bot/commands.py
+++ discord_bot/commands.py
@@ -4,10 +4,6 @@ import discord_bot.server.load_server as server
 from discord_bot.server.ClanInfo import *
 import discord
 from bot_util import *
-import os
-
-
-print("commands", os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 
 
 def is_tag_in_clan_list(entered_tag: str) -> bool:
@@ -23,12 +19,13 @@ def setup_commands(bot: commands.Bot) -> None:
     @bot.tree.command(name="add_clan")
     @app_commands.describe(tag="clan tag")
     async def display_clan(interaction: discord.Interaction, tag: str):
+        await interaction.response.defer(ephemeral=True)
         if check_if_clan_exists(tag[1:]) == True:
             clan_name = get_clan_name(tag[1:])
             clan = ClanInfo(tag=tag, clan_name=clan_name, sheet_id=None, server_id=interaction.guild_id)
             #await interaction.response.send_message(message, ephemeral=True)
             new_clan_info = server.add_clan_to_server(clan)
-            await interaction.response.send_message(f"{interaction.user.mention} your clan {new_clan_info.clan_name}({new_clan_info.tag}) has been added. \n"
+            await interaction.followup.send(f"{interaction.user.mention} your clan {new_clan_info.clan_name}({new_clan_info.tag}) has been added. \n"
                                             f"This is your clan spreadsheet link : https://docs.google.com/spreadsheets/d/{new_clan_info.sheet_id}/edit", ephemeral=True)
         else:
             await interaction.followup.send(f"Clan {tag} does not exist", ephemeral=True)
